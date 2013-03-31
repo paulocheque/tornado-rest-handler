aef188cf8ac80010852cccc8db63ea163b5c148e
paulocheque <paulocheque@gmail.com>
2013-03-31 04:30:27 -0300

    [release] 0.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='tornado-rest-handler',
           'Topic :: Software Development'
       ],
 
-      version='0.0.2',
+      version='0.0.3',
       install_requires=install_requires,
       tests_require=tests_require,
       # test_suite='runtests.runtests',
