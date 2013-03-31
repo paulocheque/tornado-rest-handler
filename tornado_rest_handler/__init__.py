c9b3b9fb80ee0f741556ac737ec82fdba37b821d
paulocheque <paulocheque@gmail.com>
2013-03-31 01:51:24 -0300

    [refactoring] parameters renamed to lower case. created CrudHandler class. using redirect pos action. using regular expression instead of ends with in url recognizer.

--- tornado_rest_handler/__init__.py
+++ tornado_rest_handler/__init__.py
@@ -1,49 +1,44 @@
 # coding: utf-8
+import re
+
 import tornado.web
 
 
-class RestHandlerMetaclass(type):
+class CrudHandlerMetaclass(type):
     def __init__(cls, name, bases, attrs):
-        result = super(RestHandlerMetaclass, cls).__init__(name, bases, attrs)
-        if attrs.get('__metaclass__') is not RestHandlerMetaclass:
+        result = super(CrudHandlerMetaclass, cls).__init__(name, bases, attrs)
+        if attrs.get('__metaclass__') is not CrudHandlerMetaclass:
             # TODO
             # cls.fields = cls.model._fields.keys()
             # cls.exclude = []
             if cls.model:
-                cls.TEMPLATE_PATH = cls.model.__name__.lower() + '/'
+                cls.template_path = cls.model.__name__.lower() + '/'
         return result
 
     def __call__(cls, *args):
-        result = super(RestHandlerMetaclass, cls).__call__(*args)
+        result = super(CrudHandlerMetaclass, cls).__call__(*args)
         if not cls.model:
             raise NotImplementedError('RestHandler classes (%s) requires the field "model".' % cls.__name__)
         return result
 
 
-class RestHandler(tornado.web.RequestHandler):
-    __metaclass__ = RestHandlerMetaclass
+class CrudHandler(tornado.web.RequestHandler):
+    __metaclass__ = CrudHandlerMetaclass
     model = None
-    TEMPLATE_PATH = ''
-    LIST_TEMPLATE = 'list.html'
-    EDIT_TEMPLATE = 'edit.html'
-    SHOW_TEMPLATE = 'show.html'
-
-    def instance_list(self): return []
-    def find_instance_by_id(self, obj_id): pass
-    def save_instance(self, obj): pass
-    def update_instance(self, obj): pass
-    def delete_instance(self, obj): pass
-
-    def obj(self, obj_id, fail_silently=False):
-        try:
-            return self.find_instance_by_id(obj_id)
-        except Exception as e:
-            if fail_silently:
-                return None
-            self.raise404()
+    template_path = ''
+    list_template = 'list.html'
+    edit_template = 'edit.html'
+    show_template = 'show.html'
+    redirect_pos_action = None
 
     def render(self, template_name, **kwargs):
-        super(RestHandler, self).render(self.TEMPLATE_PATH + template_name, **kwargs)
+        super(CrudHandler, self).render(self.template_path + template_name, **kwargs)
+
+    def raise403(self):
+        raise tornado.web.HTTPError(403, 'Not enough permissions to perform this action')
+
+    def raise404(self):
+        raise tornado.web.HTTPError(404, 'Object not found')
 
     def get_request_data(self):
         data = {}
@@ -51,63 +46,109 @@ class RestHandler(tornado.web.RequestHandler):
             data[arg] = self.get_argument(arg)
         return data
 
-    def render_list(self, alert=None):
-        self.render(self.LIST_TEMPLATE, objs=self.instance_list(), alert=alert)
+    def page_list(self, alert=None):
+        self.render(self.list_template, objs=self.instance_list(), alert=alert)
 
-    def render_edit(self, instance, errors=[], alert=None):
-        self.render(self.EDIT_TEMPLATE, obj=instance, errors=errors, alert=alert)
+    def page_new(self):
+        self.render(self.edit_template, obj=None, errors=[], alert=None)
 
-    def render_show(self, instance):
-        self.render(self.SHOW_TEMPLATE, obj=instance)
+    def page_show(self, instance):
+        self.render(self.show_template, obj=instance)
 
-    def raise404(self):
-        raise tornado.web.HTTPError(404, 'Object not found')
+    def page_edit(self, instance, errors=[], alert=None):
+        self.render(self.edit_template, obj=instance, errors=errors, alert=alert)
 
-    def get(self, obj_id=None, edit=False):
-        if self.request.uri.endswith('/new'):
-            self.render_edit(None)
-        if obj_id:
-            instance = self.obj(obj_id, fail_silently=True)
-            if instance:
-                if edit:
-                    return self.render_edit(instance)
-                else:
-                    return self.render_show(instance)
-            else:
-                return self.render_list('Object not found.')
+    def redirect_with_message(self, message=None):
+        if self.redirect_pos_action:
+            self.redirect(redirect_pos_action)
         else:
-            return self.render_list()
+            self.redirect('/')
 
-    def post(self, obj_id=None, action=None):
-        if obj_id and self.request.uri.endswith('/delete'):
-            return self.delete(obj_id)
-        if obj_id:
-            return self.put(obj_id)
+    def action_create(self):
         try:
             data = self.get_request_data()
             self.save_instance(data)
-            return self.render_list('Object added successfully.')
+            return self.redirect_with_message(message='Object added successfully.')
         except AssertionError as e:
             # TODO: capture errors to send to form
-            return self.render_edit(instance, errors=[], alert='Data sent contains some issues.')
+            return self.page_edit(instance, errors=[], alert='Data sent contains some issues.')
 
-    def put(self, obj_id):
+    def action_read(self, model_id, fail_silently=False):
+        try:
+            return self.find_instance_by_id(model_id)
+        except AssertionError as e:
+            if fail_silently:
+                return None
+            self.raise404()
+
+    def action_update(self, model_id):
         try:
             data = self.get_request_data()
-            instance = self.obj(obj_id)
+            instance = self.action_read(model_id)
             self.update_instance(instance, data)
-            return self.render_list('Object updated successfully.')
+            return self.redirect_with_message(message='Object updated successfully.')
         except AssertionError as e:
             # TODO: capture errors to send to form
-            return self.render_edit(instance, errors=[], alert='Data sent contains some issues.')
+            return self.page_edit(instance, errors=[], alert='Data sent contains some issues.')
 
-    def delete(self, obj_id):
-        instance = self.obj(obj_id)
+    def action_delete(self, model_id):
+        instance = self.action_read(model_id)
         try:
             self.delete_instance(instance)
-            return self.render_list('Object could not be deleted.')
+            return self.redirect_with_message(message='Object deleted successfully.')
         except:
-            return self.render_list('Object deleted successfully.')
+            return self.page_list('Object could not be deleted.')
+
+    def instance_list(self): return []
+    def find_instance_by_id(self, model_id): pass
+    def save_instance(self, obj): pass
+    def update_instance(self, obj): pass
+    def delete_instance(self, obj): pass
+
+
+class RestHandler(CrudHandler):
+    '''
+    GET    /animals            Index display a list of all animals
+    GET    /animals/new        New return an HTML form for creating a new animal
+    POST   /animals            Create create a new animal
+    GET    /animals/:id        Show show an animal
+    GET    /animals/:id/edit   Return an HTML form for editing a photo
+    PUT    /animals/:id        Update an animal data
+    DELETE /animals/:id        Delete an animal
+
+    POST   /animals/:id/delete Same as DELETE /animals/:id
+    POST   /animals/:id        Same as PUT /animals/:id
+
+    Since HTML5-forms does not support PUT/DELETE. It is possible to use those two methods above.
+    '''
+
+    def get(self, model_id=None, edit=False):
+        if re.match('^/.+/new', self.request.uri):
+            self.page_new()
+        if model_id:
+            instance = self.action_read(model_id, fail_silently=True)
+            if instance:
+                if edit:
+                    return self.page_edit(instance)
+                else:
+                    return self.page_show(instance)
+            else:
+                return self.page_list('Object not found.')
+        else:
+            return self.page_list()
+
+    def post(self, model_id=None, action=None):
+        if model_id and re.match('^/.+/delete', self.request.uri):
+            self.action_delete(model_id)
+        if model_id:
+            return self.action_update(model_id)
+        return self.action_create()
+
+    def put(self, model_id):
+        self.action_update(model_id)
+
+    def delete(self, model_id):
+        self.action_delete(model_id)
 
 
 class MongoEngineRestHandler(RestHandler):
@@ -127,5 +168,4 @@ class MongoEngineRestHandler(RestHandler):
 
     def delete_instance(self, instance):
         instance.delete()
-
 
