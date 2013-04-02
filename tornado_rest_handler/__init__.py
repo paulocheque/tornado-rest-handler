71f8b4e4f7e9d0cd8bdbfb0bf69876dab38572a6
paulocheque <paulocheque@gmail.com>
2013-04-02 00:28:09 -0300

    [bugfix] compatibility with the following change in Tornado 3.0: Empty HTTP request arguments are no longer ignored. This applies to HTTPRequest.arguments and RequestHandler.get_argument[s] in WSGI and non-WSGI modes.

--- tornado_rest_handler/__init__.py
+++ tornado_rest_handler/__init__.py
@@ -44,6 +44,8 @@ class CrudHandler(tornado.web.RequestHandler):
         data = {}
         for arg in list(self.request.arguments.keys()):
             data[arg] = self.get_argument(arg)
+            if data[arg] == u'': # Tornado 3.0+ compatibility
+                data[arg] = None
         return data
 
     def page_list(self, alert=None):
